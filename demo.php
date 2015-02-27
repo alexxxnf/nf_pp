dd59ee3f5fed9ebc210822183735e5bb36644fdb
Aleksey Maydokin <amaydokin@gmail.com>
2015-02-26 20:40:51 -0500

    GitHub ribbon

--- demo.php
+++ demo.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-	<title>pp</title>
+	<title>Pretty print_r</title>
 </head>
 <body>
+<a href="https://github.com/alexxxnf/nf_pp/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 <?
 include 'nf_pp.php';
 
